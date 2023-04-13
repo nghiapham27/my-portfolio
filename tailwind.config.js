6a1132d16f5fe3d04a8ad4b6280df2847e38c119
nghiapham27 <nghiaph27@gmail.com>
2023-04-13 15:16:55 +0700

    style: config tailwindcss theme and components

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {},
+      fontFamily: {
+        titleFont: ['Montserrat', 'sans-serif'],
+        bodyFont: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        bodyColor: '#1e293b',
+        primaryColor: '#38bdf8',
+        lightColor: '#f8fafc',
+        darkColor: '#94a3b8',
+        hoverColor: '#64748b',
+      },
+    },
   },
   plugins: [],
 };
